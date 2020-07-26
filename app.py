09cb0798c3adc987a4e9a608656326746924d00d
Ayushi0303 <39647711+Ayushi0303@users.noreply.github.com>
2020-07-26 13:14:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "is cursed"
+	new_name = str(name) + " is cursed"
 	return render_template('index.html', name=new_name)
 
 
